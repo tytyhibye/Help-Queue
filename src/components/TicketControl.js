20e59a23015f185b6fdcd0c4f3a82b7d603b8b75
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-07-01 08:30:39 -0700

    add new ticket

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -12,10 +12,19 @@ class TicketControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
+      masterTicketlist: [],
       counter: 0
 
     };
 
+  }
+
+  handleAddingNewTicketToList = (newTicket) => {
+    const newsMasterTicketList = this.state.masterTicketlist.concat(newTicket);
+    this.setState({
+      masterTicketlist: newsMasterTicketList,
+      counter: 0
+    });
   }
 
   handleClick = () => {
@@ -36,20 +45,20 @@ class TicketControl extends React.Component {
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.counter == 0) {
-      currentlyVisibleState = <TicketList />
+    if (this.state.counter === 0) {
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketlist} />
       buttonText = "Add ticket!";
-    } else if (this.state.counter == 1) {
+    } else if (this.state.counter === 1) {
       currentlyVisibleState = <Help />
       buttonText = "Yes I've done the steps";
-    } else if (this.state.counter == 2) {
+    } else if (this.state.counter === 2) {
       currentlyVisibleState = <Steps />
       buttonText = "Help me";
-    } else if (this.state.counter == 3) {
+    } else if (this.state.counter === 3) {
       currentlyVisibleState = <Fifteen />
       buttonText = "Please, I've done it all";
-    } else if (this.state.counter == 4) {
-      currentlyVisibleState = <NewTicketForm />
+    } else if (this.state.counter === 4) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
       buttonText = "Return to List";
     }
 
